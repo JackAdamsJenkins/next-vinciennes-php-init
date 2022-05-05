1c954aa350bbb49431febbdd15c5c132cbaad665
JackAdamsJenkins <clensx@gmail.com>
2022-05-05 14:49:57 +0200

    Cours PHP - 05-05-2022

---
+++ exercices.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+    EX 01 :
+        Vous allez créer un code qui permet de saisir un prix HT (simulé) et d'afficher le prix TTC correspondant
+
+    TVA : 20%
+
+    Concrètement : 
+        - Si le prix HT est de 100€
+        - Le prix TTC sera 120€
+*/
+$prixHT = 200;
+
+
+/*
+    EX 01 :
+    Créer un programme qui demande d’entrer une note (simulée) et qui affiche :
+        - Reçu avec mention assez bien, si la note est supérieure ou égale à 12
+        - Reçu avec mention passable, si la note est supérieure à 10 et inférieure à 12
+        - Insuffisant, dans tous les autres cas
+*/
+$note = 8;
+?>
