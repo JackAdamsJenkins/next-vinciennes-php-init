900c256f05dd57ea9095731a6cb5235a944afdbc
JackAdamsJenkins <clensx@gmail.com>
2022-05-06 11:26:33 +0200

    Exercice à jour avec boucle while

--- exercices2.php
+++ exercices2.php
@@ -6,15 +6,26 @@
         echo "<br/>Nombre pair : $i";
     }
 
-    // $variable += 20; // equivalent à la ligne du dessous
-    // $variable = $variable - 20;
+    // $i = $i + 2;
+    // $i += 2; // équivalent à la ligne au dessus
 
 
     /*
         Avec la boucle for, afficher tous les nombres impairs de 0 à 88 
     */
-    for($i = 1; $i <= 88; $i += 2){
+    for($i = 1; $i <= 88; $i = $i + 2){
         echo "<br/>Nombre impair : $i";
     }
+
+
+    /*
+        Avec la boucle while, afficher tous les nombres pairs de 0 à 50 (inclu)
+    */
+
+
+    /*
+        Avec la boucle while, afficher tous les nombres impairs de 0 à 88 
+    */
+
 
 ?>
