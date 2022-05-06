51a05edf4b9b2f3f167b6c2719a238b9b7e00b29
JackAdamsJenkins <clensx@gmail.com>
2022-05-06 11:06:29 +0200

    Correction exercices for

--- exercices2.php
+++ exercices2.php
@@ -2,12 +2,19 @@
     /*
         Avec la boucle for, afficher tous les nombres pairs de 0 à 50 (inclu)
     */
+    for($i = 0; $i <= 50; $i += 2){
+        echo "<br/>Nombre pair : $i";
+    }
 
+    // $variable += 20; // equivalent à la ligne du dessous
+    // $variable = $variable - 20;
 
 
     /*
-        Avec la boucle for, afficher tous les nombres impairs de 0 à 88 (inclu)
+        Avec la boucle for, afficher tous les nombres impairs de 0 à 88 
     */
-
+    for($i = 1; $i <= 88; $i += 2){
+        echo "<br/>Nombre impair : $i";
+    }
 
 ?>
