a733bd89e75249da35183649685bde110ec8b026
JackAdamsJenkins <clensx@gmail.com>
2022-05-06 10:27:00 +0200

    Ajout des exercices sur les boucles for

---
+++ exercices2.php
@@ -0,0 +1,13 @@
+<?php 
+    /*
+        Avec la boucle for, afficher tous les nombres pairs de 0 à 50 (inclu)
+    */
+
+
+
+    /*
+        Avec la boucle for, afficher tous les nombres impairs de 0 à 88 (inclu)
+    */
+
+
+?>
