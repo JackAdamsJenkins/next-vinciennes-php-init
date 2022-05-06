9d4f0c70ec4219a0cab991c48d66f2ad8da6c9ac
JackAdamsJenkins <clensx@gmail.com>
2022-05-06 16:56:40 +0200

    Correction exercice

--- exercices3.php
+++ exercices3.php
@@ -2,11 +2,21 @@
     /*
         Créez une fonction qui permet d'afficher simplement le contenu d'une variable transmise
     */
+    function afficheContenu($maVariable){
+        echo $maVariable;
+    }
+
+    afficheContenu("Bonjour à tous !");
     
     
     /*
         Créez une fonction qui permet de multiplier deux nombres et RETOURNER (return) son résultat
     */
+    function multiplier($nb1 = 0, $nb2 = 0){
+        return $nb1 * $nb2;
+    }
+
+    echo "<br/>".multiplier(6, 2);
 
     /*
         Créez une fonction qui permet de prendre 3 paramètres :
@@ -21,6 +31,11 @@
         2e info = 2
         3e info = 2
     */
+    function renvoiResultat($nb1 = 0, $nb2 = 0, $nb3 = 0){
+        return ($nb1 + $nb2) * $nb3;
+    }
+
+    echo "<br/>Résultat du calcul : ".renvoiResultat(10, 20, 2);
 
 
 ?>
