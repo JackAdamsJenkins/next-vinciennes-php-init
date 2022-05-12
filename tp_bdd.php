900ae4bfcae9ab7c7d59e85495f14ea519427f89
JackAdamsJenkins <clensx@gmail.com>
2022-05-12 14:48:48 +0200

    Consigne exercice BDD

---
+++ tp_bdd.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+    Vous allez créer une nouvelle TABLE dans votre base de données
+    Cette table devra contenir les informations sur des voitures
+    Vous allez devoir stocker les données suivantes :
+        - id de données
+        - Marque de la voiture (Renault, Peugeot, Mercedes..)
+        - Modèle de la voiture (Clio, Twingo, etc.)
+        - L'année de fabrication du véhicule
+        - Couleur du véhicule (Rouge, bleu, vert)
+
+    Une fois la table créée, vous allez devoir "peupler" les données
+    Pour cela, vous allez laisser l'utilisateur remplir les données
+    Vous allez donc mettre à disposition de l'utilisateur, un formulaire permettant
+    de renseigner ces données et bien évidemment de les ajouter à la base de données
+
+    Une fois qu'une donnée est enregistrée, n'oubliez pas d'afficher un message à l'utilisateur
+    pour lui dire que tout s'est bien passé.
+
+    ENSUITE ----
+    Dessous le formulaire, vous allez ajouter un h2 avec pour titre "Liste des véhicules"
+    Et bien évidemment, sous ce h2, vous allez devoir récupérer et afficher la liste de TOUS les véhicules présents
+    dans la base de données
+
+
+    ENSUITE ---
+    Dessous la liste de TOUS les véhicules, ajouter un nouveau h2 avec pour titre "Les derniers véhicules"
+    A cet endroit, vous allez afficher les 4 derniers véhicules enregistrés dans la base de données
+
+*/
+
+
+?>
